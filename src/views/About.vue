433f83be6bc5cc7213bd06c3a0c5d062e6b83cea
Alexandra Maceda <alexandramaceda14@gmail.com>
2020-10-21 03:57:41 -0400

    Pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-app>
+    
+    <v-card width="50%" class="mx-auto mt-15">
+      <v-card-title>
+        <h1 class="display-1"> About</h1>
+      </v-card-title>
+      <v-card-text>
+        App that tracks exercises and allows you to share with friends!
+      </v-card-text>
+    </v-card>
+  </v-app>
 </template>
+<script>
+  export default {
+    name: 'About',
+
+    data: () => ({ }),
+  }
+</script>
---
+++ src/views/AdminPage.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-app>
+        <v-card>
+        <v-card-title> 
+            <h1> USERS</h1>
+            </v-card-title>
+            </v-card> 
+            <v-simple-table>
+              <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Username
+          </th>
+          <th class="text-left">
+            Password
+          </th>
+          <th> ADD/DELETE</th>
+        </tr>
+        
+      </thead>
+      <tbody>
+        <tr>
+          <td>Bob12</td>
+          <td>Lfjj12.</td>
+        </tr>
+      </tbody>
+    </template>
+    </v-simple-table>
+    </v-app>
+   
+</template>
+<script>
+
+// @ is an alias to /src
+import AdminPage from './AdminPage.vue'
+
+export default {
+  name: 'AdminPage',
+  components: {
+    AdminPage
+  }
+}
+</script>
